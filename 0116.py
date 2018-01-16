5524cb24d7bb389744c85d8ceffc393cc7b660dc
strong-maronu <35374786+strong-maronu@users.noreply.github.com>
2018-01-16 09:38:35 +0900

    Add files via upload

--- 0116.py
+++ 0116.py
@@ -6,20 +6,20 @@ import sys, codecs, csv
 kanawords = {}
 tweets = {}
 
-for row in codecs.open("dictionary.txt","r","shift_jis"):
+for row in open("dictionary.txt","r"):
     row0 = row.rstrip().split("\t")
     kanawords[row0[0]] = row0[1]
-    print row0[0], row0[1]
 
-f = open("abeshinzo.txt.chasen","w+")
+f = open("abeshinzo.txt.chasen","r")
 for row in f:
     row = row.rstrip()
-    if row == "EOS":
+    if row[0] == "EOS":
         pass
     else:
-        for word in kanawords[0]:
-            if word in row:
-                row = word
+        for word in kanawords:
+            if word in row[0]:
+                row[0] = word
             else:
                 pass
-
+        f2 = open("censored.txt","a")
+        f2.write(row[0])
